c7f05757c0af4aa12f27561118a5bfa2f4a2e647
Dhoni Eko Wahyu Nugroho <studiokoding@gmail.com>
2024-10-03 16:17:39 +0700

    perubahan layout

--- backend/protected/views/layouts/login.php
+++ backend/protected/views/layouts/login.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Login | Attex - Responsive Tailwind CSS 3 Admin Dashboard</title>
+    <title>Login | Backend Konsultasi Regional PDRB Jabalnusra</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc., Tailwind, TailwindCSS, Tailwind CSS 3" name="description">
     <meta content="coderthemes" name="author">
--- backend/protected/views/layouts/main.php
+++ backend/protected/views/layouts/main.php
@@ -211,7 +211,7 @@
                 <!-- Profile Dropdown Button -->
                 <div class="relative">
                     <button data-fc-type="dropdown" data-fc-placement="bottom-end" type="button" class="nav-link flex items-center gap-2.5 px-3 bg-black/5 border-x border-black/10">
-                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/users/avatar-1.jpg" alt="user-image" class="rounded-full h-8">
+                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/users/user.webp" alt="user-image" class="rounded-full h-8">
                         <span class="md:flex flex-col gap-0.5 text-start hidden">
                             <h5 class="text-sm"><?php echo Yii::app()->user->name;?></h5>
                             <span class="text-xs"><?php echo Yii::app()->user->satker;?></span>
@@ -219,11 +219,6 @@
                     </button>
 
                     <div class="fc-dropdown fc-dropdown-open:opacity-100 hidden opacity-0 w-44 z-50 transition-all duration-300 bg-white shadow-lg border rounded-lg py-2 border-gray-200 dark:border-gray-700 dark:bg-gray-800">
-                        <!-- item-->
-                        <a href="<?php echo Yii::app()->createUrl('user/profile',array('id'=>5));?>" class="flex items-center gap-2 py-1.5 px-4 text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
-                            <i class="ri-account-circle-line text-lg align-middle"></i>
-                            <span>My Account</span>
-                        </a>
                         <!-- item-->
                         <a href="<?php echo Yii::app()->createUrl('site/logout');?>" class="flex items-center gap-2 py-1.5 px-4 text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
                             <i class="ri-logout-box-line text-lg align-middle"></i>
@@ -410,7 +405,6 @@
 
             <div class="h-16 p-4 flex items-center gap-4 border-t border-gray-300 dark:border-gray-600 px-6">
                 <button type="button" class="btn bg-primary text-white w-1/2" id="reset-layout">Reset</button>
-                <button type="button" class="btn bg-light text-dark dark:text-light dark:bg-opacity-10 w-1/2">Buy Now</button>
             </div>
         </div>
     </div>
